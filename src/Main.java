d894c6dde051629deaeb88915e830131e7a44e87
Patryk Tomaszewski <ptomaszewskig3@gmail.com>
2020-04-05 14:35:39 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    /*
+    PREZ4
+Ekstensja
+ Ekst. - trwałość
+ Atr. złożony
+ Atr. opcjonalny
+ Atr. powt.
+ Atr. klasowy
+ Atr. pochodny
+ Met. klasowa
+ Przesłonięcie, przeciążenie
+    */
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
